f66ee4626b94910826fb046021eb6c60ef3082d2
antalszava <antalszava@gmail.com>
2020-12-09 11:30:35 -0500

    Latest PL

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 with open("pennylane_orquestra/_version.py") as f:
     version = f.readlines()[-1].split()[-1].strip("\"'")
 
-requirements = ["pyyaml", "pennylane>=0.11"]
+requirements = ["pyyaml", "pennylane>=0.13"]
 
 info = {
     'name': 'PennyLane-Orquestra',
