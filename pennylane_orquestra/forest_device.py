565167528a9367ea1a89f8291c019d98cc84c670
antalszava <antalszava@gmail.com>
2020-12-14 21:35:42 -0500

    Forest qvm

--- pennylane_orquestra/forest_device.py
+++ pennylane_orquestra/forest_device.py
@@ -27,4 +27,13 @@ class QeForestDevice(OrquestraDevice):
     qe_function_name = "ForestSimulator"
 
     def __init__(self, wires, shots=1024, backend="wavefunction-simulator", **kwargs):
+        if "qvm" in backend:
+            if kwargs.get("analytic", False):
+                # Raise a warning if the analytic attribute was set to True
+                warnings.warn(
+                    f"The {backend} backend device cannot be used in "
+                    "analytic mode. Results are based on sampling."
+                )
+
+            kwargs["analytic"] = False
         super().__init__(wires, backend=backend, shots=shots, **kwargs)
