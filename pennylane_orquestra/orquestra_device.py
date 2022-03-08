885606085ed0a7f507933227b9fd25a366e3f819
Antal Szava <antalszava@gmail.com>
2022-03-08 16:28:15 -0500

    Use qml.matrix

--- pennylane_orquestra/orquestra_device.py
+++ pennylane_orquestra/orquestra_device.py
@@ -21,7 +21,7 @@ import json
 import re
 import uuid
 
-from pennylane import QubitDevice
+from pennylane import QubitDevice, matrix
 from pennylane.operation import Expectation, Tensor
 from pennylane.ops import Identity
 from pennylane.utils import decompose_hamiltonian
@@ -360,7 +360,7 @@ class OrquestraDevice(QubitDevice, abc.ABC):
             # Decompose the matrix of the observable
             # This removes information about the wire labels used and
             # consecutive integer wires are used
-            coeffs, obs_list = decompose_hamiltonian(qml.matrix(original_observable))
+            coeffs, obs_list = decompose_hamiltonian(matrix(original_observable))
 
             for idx in range(len(obs_list)):
                 obs = obs_list[idx]
