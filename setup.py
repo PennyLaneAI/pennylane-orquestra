201718cd5f755719130968b5ef08cccd8de65148
Jay Soni <jbsoni@uwaterloo.ca>
2022-12-13 10:48:46 -0500

    update pl_version in requirements

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import os
 with open("pennylane_orquestra/_version.py") as f:
     version = f.readlines()[-1].split()[-1].strip("\"'")
 
-requirements = ["pyyaml", "pennylane>=0.15"]
+requirements = ["pyyaml", "git+https://github.com/PennyLaneAI/pennylane#egg=pennylane"]
 
 info = {
     'name': 'PennyLane-Orquestra',
